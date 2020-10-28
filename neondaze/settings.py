619b1b356b3f3a023b4ea00f3db015cc864e68d8
mxbialostocki <matt.bialostocki@gmail.com>
2020-10-29 08:53:02 +1300

    postgresql integration 01

--- neondaze/settings.py
+++ neondaze/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'neondaze.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'django',
+        'USER': 'postgres',
+        'PASSWORD': 'candle',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
@@ -105,7 +109,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Pacific/Auckland'
 
 USE_I18N = True
 
