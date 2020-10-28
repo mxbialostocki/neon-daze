52b4d564ff324bae81b19d3724c9cdc3edb8151f
mxbialostocki <matt.bialostocki@gmail.com>
2020-10-29 09:25:33 +1300

    magic

--- neondaze/settings.py
+++ neondaze/settings.py
@@ -10,6 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
+from secrets import magic
+
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '$dmp@4%7_=#*tn6y)erdndh)c46e7!nuv$f98x*=0dy)^mp87f'
+SECRET_KEY = magic
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
