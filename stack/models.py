d53e3a7548473fd0c276bf9e7929bec5752870ba
mxbialostocki <matt.bialostocki@gmail.com>
2020-10-29 10:12:54 +1300

    develop model structure

--- stack/models.py
+++ stack/models.py
@@ -1,6 +1,14 @@
+import datetime
+
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
 class Example(models.Model):
   example_text = models.CharField(max_length=200)
   pub_date = models.DateTimeField('date published')
+
+  def __str__(self):
+    return self.example_text
+  def was_published_recently(self):
+    return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
