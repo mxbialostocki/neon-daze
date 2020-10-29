176bf06fbb550b1a50633ecabf4e8af2259d69e5
mxbialostocki <matt.bialostocki@gmail.com>
2020-10-29 16:41:10 +1300

    edit settings

--- neondaze/settings.py
+++ neondaze/settings.py
@@ -81,7 +81,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'postgres',
         'USER': 'postgres',
-        'PASSWORD': 'candle',
+        'PASSWORD': '',
         'HOST': 'localhost',
         'PORT': '5432',
     }
